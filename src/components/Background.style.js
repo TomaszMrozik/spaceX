9db2cbac35ac061c97a44dc38d675fe1e7f49bc6
Tomasz Mrozik <tomaszmrozik2000@gmail.com>
2021-10-26 16:18:27 +0200

    styled-components library and background component added

---
+++ src/components/Background.style.js
@@ -0,0 +1,8 @@
+import styled from "styled-components";
+
+export const Background = styled.body`
+  width: 100%;
+  height: 100vh;
+  background: linear-gradient(297.9deg, #323237 15.11%, #000000 90.89%);
+  background-attachment: scroll;
+    `;
